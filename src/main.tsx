bdb76ef5df3c856cf859fb19d9803695394b36aa
Chien Dang <chien.dang@digitalfortress.dev>
2025-06-11 09:06:37 +0700

    init source

---
+++ src/main.tsx
@@ -0,0 +1,35 @@
+import { StrictMode } from "react";
+import ReactDOM from "react-dom/client";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { RouterProvider, createRouter } from "@tanstack/react-router";
+import "./index.css";
+import { routeTree } from "./routerTree.gen";
+
+const queryClient = new QueryClient();
+
+const router = createRouter({
+  routeTree,
+  context: { queryClient },
+  defaultPreload: "intent",
+  defaultPreloadStaleTime: 0,
+});
+
+// Register the router instance for type safety
+declare module "@tanstack/react-router" {
+  interface Register {
+    router: typeof router;
+  }
+}
+
+// Render the app
+const rootElement = document.getElementById("root")!;
+if (!rootElement.innerHTML) {
+  const root = ReactDOM.createRoot(rootElement);
+  root.render(
+    <StrictMode>
+      <QueryClientProvider client={queryClient}>
+        <RouterProvider router={router} />
+      </QueryClientProvider>
+    </StrictMode>
+  );
+}
---
+++ src/routerTree.gen.ts
@@ -0,0 +1,81 @@
+import { Route as rootRoute } from "./routes/_root";
+import { Route as AuthenticatedRouteImport } from "./routes/_authenticated/route";
+import { Route as AuthenticatedIndexImport } from "./routes/_authenticated/index";
+
+const AuthenticatedRouteRoute = AuthenticatedRouteImport.update({
+  id: "/_authenticated",
+  getParentRoute: () => rootRoute,
+} as any);
+
+const AuthenticatedIndexRoute = AuthenticatedIndexImport.update({
+  id: "/",
+  path: "/",
+  getParentRoute: () => AuthenticatedRouteRoute,
+} as any);
+
+declare module "@tanstack/react-router" {
+  interface FileRoutesByPath {
+    "/_authenticated": {
+      id: "/_authenticated";
+      path: "";
+      fullPath: "";
+      preLoaderRoute: typeof AuthenticatedRouteImport;
+      parentRoute: typeof rootRoute;
+    };
+
+    "/_authenticated/": {
+      id: "/_authenticated/";
+      path: "/";
+      fullPath: "/";
+      preLoaderRoute: typeof AuthenticatedIndexImport;
+      parentRoute: typeof AuthenticatedRouteImport;
+    };
+  }
+}
+interface AuthenticatedRouteRouteChildren {
+  AuthenticatedIndexRoute: typeof AuthenticatedIndexRoute;
+}
+
+const AuthenticatedRouteRouteChildren: AuthenticatedRouteRouteChildren = {
+  AuthenticatedIndexRoute: AuthenticatedIndexRoute,
+};
+
+export interface FileRoutesByFullPath {
+  "": typeof AuthenticatedRouteRouteWithChildren;
+  "/": typeof AuthenticatedIndexRoute;
+}
+
+export interface FileRoutesByTo {
+  "": typeof AuthenticatedRouteRouteWithChildren;
+  "/": typeof AuthenticatedIndexRoute;
+}
+
+export interface FileRoutesById {
+  __root__: typeof rootRoute;
+  "/_authenticated": typeof AuthenticatedRouteRouteWithChildren;
+  "/_authenticated/": typeof AuthenticatedIndexRoute;
+}
+
+export interface FileRouteTypes {
+  fileRoutesByFullPath: FileRoutesByFullPath;
+  fullPaths: "" | "/";
+  fileRoutesByTo: FileRoutesByTo;
+  to: "/";
+  id: "__root__" | "/_authenticated";
+  fileRoutesById: FileRoutesById;
+}
+
+export interface RootRouteChildren {
+  AuthenticatedRouteRoute: typeof AuthenticatedRouteRouteWithChildren;
+}
+
+const AuthenticatedRouteRouteWithChildren =
+  AuthenticatedRouteRoute._addFileChildren(AuthenticatedRouteRouteChildren);
+
+const rootRouteChildren: RootRouteChildren = {
+  AuthenticatedRouteRoute: AuthenticatedRouteRouteWithChildren,
+};
+
+export const routeTree = rootRoute
+  ._addFileChildren(rootRouteChildren)
+  ._addFileTypes<FileRouteTypes>();
