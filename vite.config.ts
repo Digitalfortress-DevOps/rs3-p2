3b1bb531e1ca9a1ed423adcbf8776b5806eb33a3
kinhdev24 <kinhdev24@gmail.com>
2025-06-16 10:43:21 +0700

    chore: setup deploy netlify

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,14 @@
-import {defineConfig} from "vite";
-import react from "@vitejs/plugin-react";
-import path from "node:path";
-import tailwindcss from "@tailwindcss/vite";
+import { defineConfig } from "vite"
+import react from "@vitejs/plugin-react"
+import path from "node:path"
+import tailwindcss from "@tailwindcss/vite"
+import netlify from "@netlify/vite-plugin"
 
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [react(), tailwindcss(), netlify()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
-});
+})
